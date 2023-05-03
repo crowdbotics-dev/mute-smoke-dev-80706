321981e70cff2356c118996c9e6a50d208805548
Crowdbotics <engineering@crowdbotics.com>
2023-05-03 05:53:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-smoke-dev-80706",
+  url: "https://mute-smoke-dev-80706.botics.co",
+  api: "https://mute-smoke-dev-80706.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
